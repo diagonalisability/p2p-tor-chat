7d992b51a52f6e9d446c12c7f8394fe473399652
diagonalisability <Yd72MfFnJWLSyHm5@protonmail.com>
2020-12-27 01:13:11 -0500

    refactor to make gui thread the main thread for much less message-passing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 // main.rs
-enum Message {
-	PeerMessage(String/*sender*/,String/*body*/)
+#[derive(Clone)]
+struct GrowingThreadPool {
+	// idk if Mutex is overkill but it seems to do what i want
+	o: std::sync::Arc<std::sync::Mutex<threadpool::ThreadPool>>,
+}
+impl GrowingThreadPool {
+	fn new()->GrowingThreadPool {
+		GrowingThreadPool {
+			o: std::sync::Arc::new(std::sync::Mutex::new(threadpool::ThreadPool::new(4))),
+		}
+	}
+	fn execute<F:FnOnce()+Send+'static>(&self,job:F) {
+		let mut o= self.o.lock().unwrap();
+		if 0<o.queued_count() {
+			let new_count= o.max_count()+1;
+			o.set_num_threads(new_count);
+		}
+		o.execute(job);
+	}
+	fn join(&self) {
+		self.o.lock().unwrap().join();
+	}
+}
+// join if this is the last reference (ideal for cloning and passing around)
+impl Drop for GrowingThreadPool {
+	fn drop(&mut self) {
+		if std::sync::Arc::strong_count(&self.o)==1 {
+			self.join();
+		}
+	}
 }
 struct TorChild {
 	process: std::process::Child,
@@ -9,12 +37,17 @@ struct TorChild {
 		tokio::net::UnixStream,
 		fn(torut::control::AsyncEvent<'static>)->DummyFuture>
 }
+impl std::fmt::Debug for TorChild {
+	fn fmt(&self,f:&mut std::fmt::Formatter<'_>)-> Result<(),std::fmt::Error> {
+		f.write_str("TorChild{...}")
+	}
+}
 // torut never needs this, but the AuthenticatedConn struct has a weird type parameter
 // which this is used to fix.
 struct DummyFuture {}
 impl std::future::Future for DummyFuture {
 	type Output= Result<(),torut::control::ConnError>;
-	fn poll(self:std::pin::Pin<&mut Self>,cx:&mut std::task::Context<'_>)->std::task::Poll<Self::Output> {
+	fn poll(self:std::pin::Pin<&mut Self>,_cx:&mut std::task::Context<'_>)->std::task::Poll<Self::Output> {
 		panic!("this way i don't need to return anything");
 	}
 }
@@ -30,7 +63,7 @@ impl TorChild {
 				"SocksPort", "auto",//&format!("unix:{}/socks-socket",profile_dir),
 //				"CookieAuthentication","1",
 //				"CookieAuthFile","cookie",
-				"Log","info file tor-log",
+				"Log","notice file tor-log",
 				"Log","notice",
 			])
 			.current_dir(profile_dir)
@@ -60,7 +93,6 @@ impl TorChild {
 		}
 	}
 	fn wait_for_ready(lines:&mut std::io::Lines<std::io::BufReader<std::process::ChildStdout>>) {
-		use std::io::BufRead;
 		loop {
 			if lines.next().unwrap().unwrap().contains("Opened Control listener") {
 				println!("control listener open");
@@ -82,7 +114,6 @@ impl TorChild {
 	async fn enable_network(&mut self)->u32 {
 		self.acon.set_conf("DisableNetwork",Some("0")).await.unwrap();
 		println!("getting socks port");
-		use std::io::BufRead;
 		loop {
 			let line= self.stdout_lines.next().unwrap().unwrap();
 			if line.contains("Opened Socks listener on") {
@@ -102,66 +133,149 @@ impl Drop for TorChild {
 			.unwrap();
 	}
 }
-fn connect_to_peer(socks_port:u32) {
+fn open_chat() {
 	use gtk::prelude::*;
-	let glade_src= std::fs::read_to_string("peer-connect.glade").unwrap();
+	let glade_src= std::fs::read_to_string("chat.glade").unwrap();
 	let builder= gtk::Builder::from_string(&glade_src);
+	let window:gtk::Window= builder.get_object("window").unwrap();
+	window.show_all();
+}
+fn connect_to_peer(socks_port:u32,thread_pool:GrowingThreadPool) {
+	use gtk::prelude::*;
+	let builder= open_glade("peer-connect.glade");
 	let window:gtk::Window= builder.get_object("window0").unwrap();
 	let button:gtk::Button= builder.get_object("button0").unwrap();
 	let entry:gtk::Entry= builder.get_object("textfield0").unwrap();
-	button.connect_clicked(move/*entry*/|_but| {
-		println!("button clicked with entry text '{}'",entry.get_buffer().get_text());
-		//connect_with_address(entry.get_buffer().get_text());
-		let mut stream= socks::Socks5Stream::connect(
-			format!("127.0.0.1:{}",socks_port),
-			format!("{}:20001",entry.get_buffer().get_text().as_str()).as_str()
-		).unwrap();
-		println!("connected!");
-		std::io::Write::write(&mut stream,b"asdfasdfasdf\nbob\nhello, there!\nquit\n").unwrap();
-		let reader= std::io::BufReader::new(stream);
-		use std::io::BufRead;
-		println!("waiting for response");
-		println!("got response: {}",reader.lines().next().unwrap().unwrap());
-		// https://docs.rs/reqwest/0.10.9/reqwest/struct.Proxy.html
-		//let client= reqwest::Client::builder()
-		//	.proxy(reqwest::Proxy::all(&format!("socks5://127.0.0.1:{}",SOCKS_PORT)).unwrap())
-		//	.build().unwrap();
+	let thread_pool= std::rc::Rc::new(std::cell::Cell::new(Some(thread_pool)));
+	let window_c= window.clone();
+	button.connect_clicked(move|_but| {
+		let entry_text= entry.get_buffer().get_text();
+	//	let entry_text= std::rc::Rc::new(std::cell::Cell::new(entry.get_buffer().get_text()));
+		println!("button clicked with entry text '{}'",entry_text);
+		window_c.close();
+		open_chat();
+		thread_pool.take().unwrap().execute(move||{
+			//connect_with_address(entry.get_buffer().get_text());
+			println!("connecting to {}",entry_text);
+			let mut stream= socks::Socks5Stream::connect(
+				format!("127.0.0.1:{}",socks_port),
+				format!("{}:20001",entry_text).as_str(),
+			).unwrap();
+			println!("connected!");
+			std::io::Write::write(&mut stream,b"asdfasdfasdf\nbob\nhello, there!\nquit\n").unwrap();
+			let reader= std::io::BufReader::new(stream);
+			use std::io::BufRead;
+			println!("waiting for response");
+			println!("got response: {}",reader.lines().next().unwrap().unwrap());
+			// https://docs.rs/reqwest/0.10.9/reqwest/struct.Proxy.html
+			//let client= reqwest::Client::builder()
+			//	.proxy(reqwest::Proxy::all(&format!("socks5://127.0.0.1:{}",SOCKS_PORT)).unwrap())
+			//	.build().unwrap();
+		});
 	});
 	window.show_all();
 }
-// start gui
-fn run_gtk(
-	receiver:glib::Receiver<Message>,
-	profile_dir_sender:tokio::sync::oneshot::Sender<String>,
-	socks_port_receiver:tokio::sync::oneshot::Receiver<u32>
+fn open_glade(path:&str)->gtk::Builder {
+	let src= std::fs::read_to_string(path).unwrap();
+	gtk::Builder::from_string(&src)
+}
+fn select_profile<CB:Fn(&String)+'static>(cb:CB) {
+	use gtk::prelude::*;
+	let builder= open_glade("profile-select.glade");
+	let window:gtk::Window= builder.get_object("window0").unwrap();
+	let button:gtk::Button= builder.get_object("button0").unwrap();
+	let entry:gtk::Entry= builder.get_object("textfield0").unwrap();
+	{
+		let window= window.clone();
+		button.connect_clicked(move |_but| {
+//			profile_dir_sender.take().unwrap().send(entry.get_buffer().get_text()).unwrap();
+			println!("profile selected, moving on with gui");
+			window.close();
+			cb(&entry.get_buffer().get_text());
+//			let socks_port= tokio::runtime::Runtime::new().unwrap().block_on(async {
+//				socks_port_receiver.take().unwrap().await.unwrap()
+//			});
+//			connect_to_peer(socks_port,thread_pool.take().unwrap());
+		});
+	}
+	window.show_all();
+}
+fn listen(listener:std::net::TcpListener,thread_pool:GrowingThreadPool) {
+	let thread_pool_c= thread_pool.clone();
+	thread_pool.execute(move||{ for stream in listener.incoming() {
+		println!("got listen stream, queueing handle task");
+		thread_pool_c.execute(||{
+			println!("got a tor connection!");
+			let mut write_stream= stream.unwrap();
+			let read_stream= write_stream.try_clone().unwrap();
+			let mut reader= std::io::BufReader::new(&read_stream);
+			let mut peer_id= String::new(); std::io::BufRead::read_line(&mut reader,&mut peer_id).unwrap(); let peer_id= peer_id.trim();
+			println!("peer identifies themself as: {}",peer_id);
+			println!("authenticating peer by sending cookie");
+			// TODO (open a stream to peer_id.onion, peer should accept and send the cookie back down this stream)
+			println!("peer authenticated");
+			let mut nick= String::new(); std::io::BufRead::read_line(&mut reader,&mut nick).unwrap(); let nick= nick.trim();
+			println!("peer nickname: {}",nick);
+			let mut line= String::new();
+			loop {
+				line.clear();
+				std::io::BufRead::read_line(&mut reader,&mut line).unwrap(); let line= line.trim();
+				if line.trim()=="quit" {
+					break;
+				}
+				std::io::Write::write(&mut write_stream,b"hello!\n").unwrap();
+			}
+		});
+	}});
+	println!("done listen");
+}
+fn do_menu(
+	profile_dir:&String,
+	thread_pool:GrowingThreadPool,
+	tor_child_tx:tokio::sync::oneshot::Sender<TorChild>,
 ) {
 	use gtk::prelude::*;
+	// make profile_dir absolute so that tor can use it for a unix socket
+	let cur_dir= std::env::current_dir().unwrap().into_os_string().into_string().unwrap();
+	let profile_dir= format!("{}/{}",cur_dir,profile_dir);
+	println!("profile_dir is: {}",profile_dir);
+	let onion_key= get_onion_key(&profile_dir);
+	println!("start tor");
+	let rt= tokio::runtime::Runtime::new().unwrap();
+	let (tor_child,listener,socks_port)= rt.block_on(async {
+		let mut tor_child= TorChild::new(&profile_dir,&format!("{}/torrc",cur_dir)).await;
+		println!("connected to control port");
+		let listener= tor_child.make_onion_listener(onion_key).await;
+		let socks_port= tor_child.enable_network().await;
+		(tor_child,listener,socks_port)
+	});
+	println!("sending tor child");
+	tor_child_tx.send(tor_child).unwrap();
+	println!("starting listen");
+	listen(listener,thread_pool.clone());
+	let builder= open_glade("menu.glade");
+	let button:gtk::Button= builder.get_object("connect_button").unwrap();
+	let window:gtk::Window= builder.get_object("window").unwrap();
+	let thread_pool= std::rc::Rc::new(std::cell::RefCell::new(thread_pool));
+	button.connect_clicked(move|_but| {
+		connect_to_peer(socks_port,(*thread_pool).borrow().clone());
+	});
+	window.show_all();
+}
+fn main() {
+	let rt= tokio::runtime::Runtime::new().unwrap();
+	let thread_pool= GrowingThreadPool::new();
 	gtk::init().unwrap();
 	println!("gtk started");
-	{
-		let glade_src= std::fs::read_to_string("profile-select.glade").unwrap();
-		let builder= gtk::Builder::from_string(&glade_src);
-		let window:gtk::Window= builder.get_object("window0").unwrap();
-		let button:gtk::Button= builder.get_object("button0").unwrap();
-		let entry:gtk::Entry= builder.get_object("textfield0").unwrap();
-		{
-			// this is horrible but it works
-			let profile_dir_sender= std::rc::Rc::new(std::cell::Cell::new(Some(profile_dir_sender)));
-			let socks_port_receiver= std::rc::Rc::new(std::cell::Cell::new(Some(socks_port_receiver)));
-			let window= window.clone();
-			button.connect_clicked(move |_but| {
-				profile_dir_sender.take().unwrap().send(entry.get_buffer().get_text()).unwrap();
-				println!("profile selected, moving on with gui");
-				window.close();
-				let socks_port= tokio::runtime::Runtime::new().unwrap().block_on(async {
-					socks_port_receiver.take().unwrap().await.unwrap()
-				});
-				connect_to_peer(socks_port);
-			});
-		}
-		window.show_all();
-	}
-	receiver.attach(
+	let thread_pool_rc= std::rc::Rc::new(std::cell::Cell::new(Some(thread_pool.clone())));
+	let (tor_child_tx,tor_child_rx)= tokio::sync::oneshot::channel();
+	let tor_child_tx= std::rc::Rc::new(std::cell::Cell::new(Some(tor_child_tx)));
+	select_profile(move|prof|{do_menu(
+		prof,
+		(*thread_pool_rc).take().unwrap(),
+		(*tor_child_tx).take().unwrap(),
+	);});
+/*	receiver.attach(
 		None/*use thread-default context*/,
 		|msg:Message| {
 			println!("got a message");
@@ -172,32 +286,15 @@ fn run_gtk(
 			}
 			glib::Continue(true)
 		}
-	);
+	);*/
 	gtk::main();
+	// keep tor_child here for RAII when gtk::main unblocks
+	let _tor_child= rt.block_on(tor_child_rx);
+	thread_pool.join();
 }
-#[tokio::main] // check size
-async fn main() {
-	let (profile_dir,sender,gtk_thread,socks_port_sender)= {
-		// https://doc.rust-lang.org/std/sync/struct.Condvar.html
-		let (msg_sender,msg_receiver)= glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-		let (profile_dir_sender,profile_dir_receiver)= tokio::sync::oneshot::channel();
-		let (socks_port_sender,socks_port_receiver)= tokio::sync::oneshot::channel();
-		let gtk_thread= std::thread::spawn(move||{run_gtk(msg_receiver,profile_dir_sender,socks_port_receiver)});
-		(
-			profile_dir_receiver.await.unwrap(),
-			msg_sender,
-			gtk_thread,
-			socks_port_sender
-		)
-	};
-	// make profile_dir absolute so that tor can use it for a unix socket
-	let cur_dir= std::env::current_dir().unwrap().into_os_string().into_string().unwrap();
-	let profile_dir= format!("{}/{}",cur_dir,profile_dir);
-	println!("profile_dir is: {}",profile_dir);
-	// now that gtk is running, we can send events through the channel
-	//sender.send(Message::PeerMessage("bob".to_string(),"hi, alice!".to_string())).unwrap();
+fn get_onion_key(profile_dir:&String)->torut::onion::TorSecretKeyV3 {
 	let key_path= format!("{}/key",profile_dir);
-	let onion_key= match std::fs::File::open(&key_path) {
+	match std::fs::File::open(&key_path) {
 		Ok(file)=> {
 			println!("using previously-generated key");
 			let mut buf= [0u8;64];
@@ -219,44 +316,5 @@ async fn main() {
 			key
 		},
 		Err(_)=> panic!("error opening key file")
-	};
-	// https://github.com/teawithsand/torut/blob/master/examples/make_onion_v3.rs
-	// https://github.com/teawithsand/torut/blob/master/examples/cookie_authenticate.rs
-	// https://comit.network/blog/2020/07/02/tor-poc/
-	println!("start tor");
-	let mut tor_child= TorChild::new(&profile_dir,&format!("{}/torrc",cur_dir)).await;
-	let socks_port= tor_child.enable_network().await;
-	println!("connected to control port");
-	let listener= tor_child.make_onion_listener(onion_key).await;
-	socks_port_sender.send(socks_port).unwrap();
-
-	let (sender,receiver)= std::sync::mpsc::channel::<String>();
-
-	std::thread::spawn(move||{ for stream in listener.incoming() {
-		let sender= sender.clone();
-		std::thread::spawn(move||{
-			println!("got a tor connection!");
-			let mut write_stream= stream.unwrap();
-			let read_stream= write_stream.try_clone().unwrap();
-			let mut reader= std::io::BufReader::new(&read_stream);
-			let mut peer_id= String::new(); std::io::BufRead::read_line(&mut reader,&mut peer_id).unwrap(); let peer_id= peer_id.trim();
-			println!("peer identifies themself as: {}",peer_id);
-			println!("authenticating peer by sending cookie");
-			// TODO (open a stream to peer_id.onion, peer should accept and send the cookie back down this stream)
-			println!("peer authenticated");
-			let mut nick= String::new(); std::io::BufRead::read_line(&mut reader,&mut nick).unwrap(); let nick= nick.trim();
-			println!("peer nickname: {}",nick);
-			let mut line= String::new();
-			loop {
-				line.clear();
-				std::io::BufRead::read_line(&mut reader,&mut line).unwrap(); let line= line.trim();
-				sender.send(format!("{} says: '{}'",nick,line)).unwrap();
-				if line.trim()=="quit" {
-					break;
-				}
-				std::io::Write::write(&mut write_stream,b"hello!\n").unwrap();
-			}
-		});
-	}});
-	gtk_thread.join().unwrap();
+	}
 }
